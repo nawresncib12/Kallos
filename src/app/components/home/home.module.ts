ae9ba956f124b44a46e05a01389d1d7a4e670f99
nawresncib12 <nawresncib@insat.u-carthage.tn>
2023-01-17 02:30:48 +0100

    values

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,10 +4,11 @@ import { HeroComponent } from './hero/hero.component';
 import { SharedModule } from '../shared/shared.module';
 import { AboutComponent } from './about/about.component';
 import { ProductCategoryComponent } from './product-category/product-category.component';
+import { ValuesComponent } from './values/values.component';
 
 @NgModule({
-    declarations: [HeroComponent, AboutComponent, ProductCategoryComponent],
-    exports: [HeroComponent,AboutComponent,ProductCategoryComponent],
+    declarations: [HeroComponent, AboutComponent, ProductCategoryComponent, ValuesComponent],
+    exports: [HeroComponent,AboutComponent,ProductCategoryComponent,ValuesComponent],
     imports: [CommonModule, SharedModule]
 })
 export class HomeModule {}
