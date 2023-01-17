0957427b97c8577256aea7105a76e478ffe1389a
nadineboukadida <78963245+nadineboukadida@users.noreply.github.com>
2023-01-17 17:11:18 +0100

    changes

---
+++ src/app/pages/catalog/catalog.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import ApiResponse from 'src/app/data/ApiResponse';
+import { FetcherService } from 'src/app/helpers/fetcher/fetcher.service';
+import { Product } from 'src/app/model/Product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatalogService {
+
+  constructor(private fetcherService: FetcherService) { }
+
+  getAllProducts(): Observable<ApiResponse<Product[]>> {
+    return this.fetcherService.get<Product[]>('products')
+  }
+}
